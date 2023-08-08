37cbb5b6e0c7959c5908475def5da9911605eb9d
Khalid Hassouna <khalid.hassouna@llr.in2p3.fr>
2023-08-08 06:08:34 +0200

    -F

--- untitled.py
+++ untitled.py
@@ -6,20 +6,18 @@ dict = {"Staves":[1,2,3], "Modules":[2,7,9], "Towers":[55, 67, 9]}
 # List of keys in the order you want to iterate
 keys = ["Staves", "Modules", "Towers"]
 
-# List to store the combinations where the second value equals 2
-combinations = []
+def nested_loops(dict, keys, combinations =None, values={}):
+        if combinations is None:
+            combinations = []
+        if keys:
+            current_key = keys[0]
+            remaining_keys = keys[1:]
+            for value in dict.get(current_key, []):
+                new_values = values.copy()
+                new_values[current_key] = value
+                nested_loops(dict, remaining_keys, combinations, new_values)
+        else:
+            combinations.append(values)
+        return combinations
 
-# Function to perform the nested loops
-def nested_loops(dict, keys, values={}):
-    if keys:
-        current_key = keys[0]
-        remaining_keys = keys[1:]
-        for value in dict.get(current_key, {}):
-            values[current_key] = value
-            nested_loops(dict, remaining_keys, values)
-    else:
-        # If the second value equals 2, add the values to the combinations list
-        print(values)
-
-# Call the function
-nested_loops(dict, keys)
+print(nested_loops(dict, keys))
