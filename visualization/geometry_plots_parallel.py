ad2d44e17f6d9e876b9356c29228f51cec73d715
Khalid Hassouna <khalid.hassouna@llr.in2p3.fr>
2024-10-17 19:13:33 +0200

    Better canvas printing

--- visualization/geometry_plots_parallel.py
+++ visualization/geometry_plots_parallel.py
@@ -40,8 +40,9 @@ def process_file(slcio_file, ev_start, ev_stop, k, system, collections, temp_dir
             
             for hit in calo_hits:
                 decoded_hit = decoding(id_decoder, hit, is_endcap)
-                if decoded_hit["systems"] == system and decoded_hit["staves"] == 1 and decoded_hit["modules"] == 0:
-                    x, y, z, M = decoded_hit["pos_x"], decoded_hit["pos_y"], decoded_hit["pos_z"], decoded_hit["towers"]
+                # print(decoded_hit["systems"])
+                if decoded_hit["systems"] == system:
+                    x, y, z, M = decoded_hit["pos_x"], decoded_hit["pos_y"], decoded_hit["pos_z"], decoded_hit["staves"]
                     # q = 2*J*(M-1.5) + 38
                     # r = ((x**2)+(y**2))**(0.5)
                     ntuple.Fill(array('f', [x, y, M]))
@@ -50,7 +51,8 @@ def process_file(slcio_file, ev_start, ev_stop, k, system, collections, temp_dir
     temp_file.Close()
     return temp_filename
 
-def hadd_and_plot(temp_files, output_dir, canvas, title, xaxis_title, yaxis_title, colorbar_title, plot_name, temp_dir):
+def hadd_and_plot(temp_files, output_dir, title, xaxis_title, yaxis_title, colorbar_title, plot_name, temp_dir,
+                  canvas_margins, canvas_width, canvas_height, axes_sizes, to_scale=False):
     # Use hadd to merge the temporary ntuple files
     output_filename = os.path.join(temp_dir, "final_ntuple.root")
     hadd_command = "hadd -f {} {}".format(output_filename, " ".join(temp_files))
@@ -60,8 +62,13 @@ def hadd_and_plot(temp_files, output_dir, canvas, title, xaxis_title, yaxis_titl
     merged_file = ROOT.TFile(output_filename, "READ")
     ntuple = merged_file.Get("hits")
     
-    # Draw the ntuple
+    canvas = ROOT.TCanvas("c", "3D Geometry Color Plot", canvas_width, canvas_height)
+    canvas.SetLeftMargin(canvas_margins["LeftMargin"])
+    canvas.SetRightMargin(canvas_margins["RighttMargin"])
+    canvas.SetBottomMargin(canvas_margins["BottomMargin"])
+    canvas.SetTopMargin(canvas_margins["TopMargin"])
     canvas.cd()
+    
     ntuple.SetMarkerSize(1.0)
     ntuple.SetMarkerStyle(8)
     ntuple.Draw("y:x:M", "", "colz")
@@ -69,10 +76,27 @@ def hadd_and_plot(temp_files, output_dir, canvas, title, xaxis_title, yaxis_titl
     hist = canvas.GetPrimitive("htemp")
     plotting_titles = "{};{};{}".format(title, xaxis_title, yaxis_title)
     hist.SetTitle(plotting_titles)
+    hist.SetZTitle("")
+    hist.GetXaxis().SetLabelSize(axes_sizes["x_axis_label_size"])
+    hist.GetYaxis().SetLabelSize(axes_sizes["y_axis_label_size"])
+    hist.GetXaxis().SetTitleSize(axes_sizes["x_axis_title_size"])
+    hist.GetYaxis().SetTitleSize(axes_sizes["y_axis_title_size"])
+    
+    if to_scale:
+    # Retrieve the axis limits
+        x_min = hist.GetXaxis().GetXmin()
+        x_max = hist.GetXaxis().GetXmax()
+        y_min = hist.GetYaxis().GetXmin()
+        y_max = hist.GetYaxis().GetXmax()
+        aspect_ratio = (y_max - y_min) / (x_max - x_min) # Compute the aspect ratio based on the axis ranges
+        
+        # Adjust the canvas size dynamically based on the aspect ratio
+        canvas_height = int(canvas_width * aspect_ratio)
+        canvas.SetCanvasSize(canvas_width, canvas_height)
     canvas.Update()
     
     palette = canvas.GetPrimitive("palette")
-    if (palette):
+    if palette:
         x2, y2 = palette.GetX2NDC(), palette.GetY2NDC()
         latex = ROOT.TLatex()
         latex.SetTextSize(0.03)
@@ -86,6 +110,9 @@ def hadd_and_plot(temp_files, output_dir, canvas, title, xaxis_title, yaxis_titl
     canvas.Update()
     canvas.SaveAs(os.path.join(output_dir, plot_name))
     
+    # Close the ROOT file before removing the temporary files
+    merged_file.Close()
+    
     # Ensure all temporary files are closed and then remove them
     for temp_file in temp_files:
         os.remove(temp_file)
@@ -94,7 +121,9 @@ def hadd_and_plot(temp_files, output_dir, canvas, title, xaxis_title, yaxis_titl
     # Force garbage collection
     gc.collect()
 
-def plotting(slcio_files, ev_start_list, ev_stop_list, system, collections, output_dir, canvas, title, xaxis_title, yaxis_title, colorbar_title, plot_name, temp_dir_name):
+
+def plotting(slcio_files, ev_start_list, ev_stop_list, system, collections, output_dir, title, xaxis_title, yaxis_title, colorbar_title, plot_name, temp_dir_name,
+             canvas_margins, canvas_width, canvas_height, axes_sizes, to_scale=False):
     temp_dir = os.path.join(output_dir, temp_dir_name)
     if not os.path.exists(temp_dir): os.makedirs(temp_dir)
     
@@ -105,30 +134,41 @@ def plotting(slcio_files, ev_start_list, ev_stop_list, system, collections, outp
         
         temp_files = [future.result() for future in concurrent.futures.as_completed(futures)]
     
-    hadd_and_plot(temp_files, output_dir, canvas, title, xaxis_title, yaxis_title, colorbar_title, plot_name, temp_dir)
+    hadd_and_plot(temp_files, output_dir, title, xaxis_title, yaxis_title, colorbar_title, plot_name, temp_dir,
+                  canvas_margins, canvas_width, canvas_height, axes_sizes, to_scale=False)
 
 if __name__ == "__main__":  
     out_dir = "/home/llr/ilc/hassouna/script2/CalorimeterFluxes/data/ILD/FullSim/geometry_test"
-    number_of_files = 10                 
+    number_of_files = 2                 
     files = ["/home/llr/ilc/hassouna/script2/CalorimeterFluxes/data/ILD/FullSim/slcio_data/GeV90/mu-_90/partial_{}_fullSim_mu.slcio".format(i) for i in range(number_of_files)]
     ev_start_all = [0 for _ in range(number_of_files)]
     ev_stop_all = [100 for _ in range(number_of_files)]
     ev_stop_list_edited = [LcioReader.LcioReader(slico_file).getNumberOfEvents() + ev_stop + 1 if ev_stop < 0 else ev_stop for slico_file, ev_stop in zip(files, ev_stop_all)]
     total_events_per_file = [a - b for a, b in zip(ev_stop_list_edited, ev_start_all)]
-    systemo = 30
-    system_collections = ["HcalEndcapsCollection"]
+    systemo = 22
+    system_collections = ["HcalBarrelRegCollection"]
     
-    plot_title = "Implicit Selection (Q=2J(M-1.5)+38) Geometry (system=ScHcalBarrel)"
-    x_axis_title = "R (mm)"
-    y_axis_title = "Z (mm)"
-    color_bar_title = "Q=2J(M-1.5)+38"
-    plot_name = "test.jpg"
-    temp_dir_name = "implicit"
+    #Titles and names
+    plot_title = "Staves' Geometry (system=ScHcalBarrel)"
+    x_axis_title = "X (mm)"
+    y_axis_title = "Y (mm)"
+    color_bar_title = "Staves"
+    plot_name = "staves1_HCAL.jpg"
+    temp_dir_name = "Staves2"
     
-    canv = ROOT.TCanvas("c", "3D Geometry Color Plot", 1400, 800)
+    #Canvas properties
+    canvas_width, canvas_height = 1600, 1600
+    to_scale = False
+    canvas_margins = {"LeftMargin":0.15, "RighttMargin":0.15, "BottomMargin":0.15, "TopMargin":0.15}
+    axes_sizes = {"x_axis_label_size":0.02, "y_axis_label_size":0.02, "x_axis_title_size":0.05, "y_axis_title_size":0.05}
+    
     profiler = Profile()
     profiler.enable()
-    plotting(files, ev_start_all, ev_stop_list_edited, systemo, system_collections, out_dir, canv, plot_title, x_axis_title, y_axis_title, color_bar_title, plot_name, temp_dir_name)
+    plotting(files, ev_start_all, ev_stop_list_edited, systemo, system_collections, out_dir, plot_title, x_axis_title, y_axis_title, color_bar_title, plot_name, temp_dir_name,
+             canvas_margins, canvas_width, canvas_height, axes_sizes, to_scale)
+    
+    # Delay the removal slightly to avoid the file busy error
+    time.sleep(2)  # wait for 2 seconds
     shutil.rmtree(os.path.join(out_dir, temp_dir_name))
     profiler.disable()
     profiler.print_stats(sort='cumtime')
